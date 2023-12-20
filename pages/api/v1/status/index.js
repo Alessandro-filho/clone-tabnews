2af2cf3de2d0a0589cb1b1f9eb2c61f88cad9b72
Alessandro Filho <73039131+Alessandro-filho@users.noreply.github.com>
2023-12-20 01:39:05 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT NOW();");
+  console.log(result)
   response.status(200).json({ Eu: "sou acima da média." });
 }
 
-export default status
+export default status;
