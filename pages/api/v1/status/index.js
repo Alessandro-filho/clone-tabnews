7bbb935f8e8f8dfdf01669a8886f650e0898e233
Alessandro Filho <73039131+Alessandro-filho@users.noreply.github.com>
2023-12-20 13:00:45 +0000

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT NOW();");
-  console.log(result)
+  const result = await database.query("SELECT NOW() as dia;");
+  console.log(result.rows)
   response.status(200).json({ Eu: "sou acima da média." });
 }
 
